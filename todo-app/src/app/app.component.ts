76f1bd7562b5332b3a532379d1cbb877ef692014
muhwezideo <aggrey256@gmail.com>
2025-02-14 13:08:27 +0300

    add structure

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,17 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { ListTodoComponent } from './list-todo/list-todo.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TodosService } from './services/todos.service';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  // standalone: true,
-  imports: [RouterOutlet, AddTodoComponent, ListTodoComponent],
-  // providers: [HttpClientModule],
+  standalone: true,
+  imports: [RouterOutlet, AddTodoComponent, ListTodoComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  protected todos: string[] = [];
+  protected search = '';
+  $jsonTodos: Observable<{
+    todos: { id: number; todo: 'string'; completed: boolean }[];
+  }> = new Observable();
+  value = '';
   title = 'todo-app';
+  constructor(private todoService: TodosService) {
+    this.$jsonTodos = this.todoService.getAsyncTodos();
+  }
+
+  ngOnInit() {
+    this.todos = this.todoService.getTodos();
+  }
+
+  addTodo(value: string) {
+
+  }
+
+  deleteTodo() {
+
+  }
+  handleChange(value: string) {
+
+  }
+
+  handleSearch(value: string) {
+    console.log(value, '---->')
+  }
 }
---
+++ todo-app/src/app/button-base/button-base.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[buttonBase]',
+  imports: [],
+  templateUrl: './button-base.component.html',
+  styleUrl: './button-base.component.scss'
+})
+export class ButtonBaseComponent {
+
+}
